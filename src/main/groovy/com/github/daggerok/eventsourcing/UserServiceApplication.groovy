37e936a9df203508dd61322d112929e19c7df5c3
Maksim Kostromin <daggerok@gmail.com>
2019-08-02 02:58:11 +0300

    Add kafka. | asdfasdfasdfasdfasdfasdfadsf (C) whatthecommit.com

--- src/main/groovy/com/github/daggerok/eventsourcing/UserServiceApplication.groovy
+++ src/main/groovy/com/github/daggerok/eventsourcing/UserServiceApplication.groovy
@@ -2,8 +2,11 @@ package com.github.daggerok.eventsourcing
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.stream.annotation.EnableBinding
+import org.springframework.cloud.stream.messaging.Processor
 
 @SpringBootApplication
+@EnableBinding(Processor.class)
 class UserServiceApplication {
 
     static void main(String[] args) {
